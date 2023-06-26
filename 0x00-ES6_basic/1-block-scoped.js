2ae85dcfa4af1649934b2d9e341a2bf1d1fe5457
Anto/236 <Antonymbugua1237@gmail.com>
2023-06-26 15:27:46 +0000

    task 1

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+  let task = false;
+  let task2 = true;
+
+  // eslint-disable-next-line no-empty
+  if (trueOrFalse) {
+  }
+
+  return [task, task2];
+}

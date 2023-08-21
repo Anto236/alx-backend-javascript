d92c944f126b1820585353a74ae2746dd6056ddd
Anto/236 <Antonymbugua1237@gmail.com>
2023-08-21 13:12:51 +0000

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter(line => line.trim() !== ''); // Remove empty lines
+      const headers = lines[0].split(',');
+      const fieldIndex = headers.indexOf('field');
+      const students = {};
+
+      for (let i = 1; i < lines.length; i++) {
+        const values = lines[i].split(',');
+        const field = values[fieldIndex].trim();
+
+        if (field in students) {
+          students[field].count++;
+          students[field].names.push(values[0]);
+        } else {
+          students[field] = {
+            count: 1,
+            names: [values[0]]
+          };
+        }
+      }
+
+      console.log(`Number of students: ${lines.length - 1}`);
+
+      for (const field in students) {
+        console.log(`Number of students in ${field}: ${students[field].count}. List: ${students[field].names.join(', ')}`);
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;

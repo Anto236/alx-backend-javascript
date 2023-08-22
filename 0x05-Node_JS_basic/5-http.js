c3347781a2332d4bcf0ccb0ae64b82ed2c97021a
Anto/236 <Antonymbugua1237@gmail.com>
2023-08-22 08:29:15 +0000

    task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,24 +1,24 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 1245;
 
-const app = http.createServer(async (req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    let dbInfo = 'This is the list of our students\n';
-    await countStudents(process.argv[2])
-      .then((msg) => {
-        dbInfo += msg;
-        res.end(dbInfo);
-      })
-      .catch((err) => {
-        dbInfo += err.message;
-        res.end(dbInfo);
-      });
+  res.setHeader('Content-Type', 'text/plain');
+  const { url } = req;
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      countStudents(process.argv[2], res);
+    } catch (err) {
+      res.end(err.message);
+    }
   }
 });
 
